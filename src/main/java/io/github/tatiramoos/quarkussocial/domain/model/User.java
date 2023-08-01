d89d4fdb62653c55b31a56cbf6f7e36fdaf23db4
Tati Ramos <tatiramos@ibm.com>
2023-08-01 14:13:01 -0300

    ✨ Manipulando entidades com o Panache Entity

--- src/main/java/io/github/tatiramoos/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/tatiramoos/quarkussocial/domain/model/User.java
@@ -1,12 +1,15 @@
 package io.github.tatiramoos.quarkussocial.domain.model;
 
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/io/github/tatiramoos/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/tatiramoos/quarkussocial/rest/UserResource.java
@@ -1,7 +1,10 @@
 package io.github.tatiramoos.quarkussocial.rest;
 
+import io.github.tatiramoos.quarkussocial.domain.model.User;
 import io.github.tatiramoos.quarkussocial.rest.dto.CreateUserRequest;
 
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,11 +15,20 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest ) {
+
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
         return Response.ok(userRequest).build();
     }
     @GET
     public Response listAllUsers() {
-        return Response.ok().build();
+        PanacheQuery<User> query = User.findAll();
+        return Response.ok(query.list()).build();
     }
 }
